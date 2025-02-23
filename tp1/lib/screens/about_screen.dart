976fe63c3ce9b85d40997d6d5f47c070b74b016e
Geoffroy Bruneau <ounas35@gmail.com>
2025-02-23 16:22:08 +0100

    ajout d'un logo

--- tp1/lib/screens/about_screen.dart
+++ tp1/lib/screens/about_screen.dart
@@ -8,12 +8,26 @@ class AboutScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("About Us")),
       body: Center(
-        child: Text(
-          "App’s name : MyGeekHub \nVersion : 1.0 \nCreator’s name : Geoffroy B. \nStudent in the engineering school IMT Nord Europe \nGitHub : https://github.com/ounas35/AMSE",
-          style: TextStyle(
-            fontSize: 20,
-          ),
-          textAlign: TextAlign.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo
+            Image.asset(
+              '../assets/logo_app.png', // Remplace par le bon chemin de ton logo
+              width: 300, // Ajuste la taille selon ton besoin
+              height: 300,
+            ),
+            SizedBox(height: 20), // Espace entre le logo et le texte
+
+            // Texte d'info
+            Text(
+              "App’s name : MyGeekHub \nVersion : 1.0 \nCreator’s name : Geoffroy B. \nStudent in the engineering school IMT Nord Europe \nGitHub : https://github.com/ounas35/AMSE",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
